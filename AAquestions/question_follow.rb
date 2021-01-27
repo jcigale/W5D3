1afcec66bbd792b8ecdbd2daf9a7ec6eb21e526f
Marco Torre <marco.a.torre@gmail.com>
2021-01-27 17:27:55 -0500

    Built QuestionFollow Class

--- AAquestions/question_follow.rb
+++ AAquestions/question_follow.rb
@@ -1,4 +1,11 @@
+require_relative "users"
+
 class QuestionFollow
+
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+        data.map { |datum| QuestionFollow.new(datum) }
+    end
     
     def initialize(options)
         @questions_id = options['questions_id']
@@ -6,7 +13,37 @@ class QuestionFollow
     end
 
     def self.followers_for_question_id(question_id)
-        
+        users = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+          SELECT 
+            *
+          FROM
+            users
+          JOIN 
+            questions_follows ON users.id = question_follows.users_id
+          JOIN
+            replies ON question_follows.questions_id = replies.question_id
+          WHERE
+            questions_id = replies.question_id
+        SQL
+        return nil if user.length <= 0
+        Users.new(user.first)
     end
-    
+
+    def self.followed_questions_for_user_id(user_id)
+        questions = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+          SELECT 
+            *
+          FROM
+            questions
+          JOIN 
+            questions_follows ON questions.id = question_follows.questions_id
+          JOIN
+            replies ON question_follows.questions_id = replies.question_id
+          WHERE
+            user_id = replies.author_id
+        SQL
+        return nil if questions.length <= 0
+        Questions.new(questions.first)
+    end
+
 end
